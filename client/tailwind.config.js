8642fdbb46de266dfbafca8467fe386b9598db88
Mahmoud Esmail <mahmoudesmail677@gmail.com>
2021-07-01 18:38:46 +0200

    add register page - the frontend UI part only

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: "17.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
